7099c2bee56ac2d2ba63d484f2df91d46f8002a8
Yannick Fricke <yannickfricke@googlemail.com>
2019-03-20 20:34:07 +0100

    Collect code coverage from the source files. Set up the roots for jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.test.ts',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: 'code-cov',
+  roots: [
+    'src/'
+  ]
 };
