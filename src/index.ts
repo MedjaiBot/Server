661b9e1e8d2597796b4d11f55f7d1c986c58dea5
Yannick Fricke <yannickfricke@googlemail.com>
2019-09-18 20:22:23 +0200

    refactor(application): bound the InitializationSide
    Bound the initialization side to "SERVER"

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import 'reflect-metadata';
 
 import { ContainerConstants } from '@medjaibot/framework/constants/ContainerConstants';
 import { Logger } from '@medjaibot/framework/logger/Logger';
+import { InitializationSide } from '@medjaibot/framework/plugin/InitializationSide';
 import { Application } from './Application';
 import Symbols from './constants/Symbols';
 import applicationContainer from './container/container';
+
+// Bind the initialization side
+applicationContainer.bind<InitializationSide>(
+    ContainerConstants.SYSTEMS.PLUGIN.INITIALIZATIONSIDE,
+).toConstantValue(
+    InitializationSide.SERVER,
+);
 
 try {
     const application = applicationContainer.get<Application>(Symbols.APP.APPLICATION);
